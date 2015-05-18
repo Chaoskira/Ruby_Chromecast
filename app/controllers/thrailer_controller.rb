9e3ca9ecc0704caea400b1fddee3ac090275c9ab
Gordfang <Baptiste.cazaubon@gmail.com>
2015-05-18 16:10:25 +0200

    Avancement sur les suggestions des trailers

--- app/controllers/thrailer_controller.rb
+++ app/controllers/thrailer_controller.rb
@@ -1,5 +1,9 @@
 class ThrailerController < ApplicationController
   def jouer
+  	@film_detail = Tmdb::Movie.detail(params[:id]);
+  	@film_trailer = Tmdb::Movie.trailers(params[:id]);
+  	@film_similaire = Tmdb::Movie.similar_movies(params[:id]);
+  	@film_image = Tmdb::Movie.images(params[:id]);
   end
 
   def suggestion
