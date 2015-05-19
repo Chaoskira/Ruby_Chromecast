8dd8e1f56ca01994a52ed6deb39db1f24c135f8b
Gordfang <Baptiste.cazaubon@gmail.com>
2015-05-19 10:57:59 +0200

    Ajout des variable pour controller quel trailer jouer

--- app/controllers/thrailer_controller.rb
+++ app/controllers/thrailer_controller.rb
@@ -1,9 +1,13 @@
 class ThrailerController < ApplicationController
   def jouer
-  	@film_detail = Tmdb::Movie.detail(177572);
-  	@film_trailer = Tmdb::Movie.trailers(177572);
-  	@film_similaire = Tmdb::Movie.similar_movies(177572);
-  	@film_image = Tmdb::Movie.images(177572);
+  	@film_id = 99861;
+  	@film_detail = Tmdb::Movie.detail(@film_id);
+  	@film_trailer = Tmdb::Movie.trailers(@film_id);
+  	@film_similaire = Tmdb::Movie.similar_movies(@film_id);
+  	@film_image = Tmdb::Movie.images(@film_id);
+  	@num_trailer = 2;
+  	@num = 0;
+  	@source = "";
   end
 
   def suggestion
