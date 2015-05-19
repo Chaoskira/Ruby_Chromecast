04e985b5520bd4155943e32269564cfc06288931
Gordfang <Baptiste.cazaubon@gmail.com>
2015-05-19 14:13:47 +0200

    Changement de l'algorithme qui affiche les trailers

--- app/controllers/thrailer_controller.rb
+++ app/controllers/thrailer_controller.rb
@@ -1,13 +1,11 @@
 class ThrailerController < ApplicationController
   def jouer
-  	@film_id = 99861;
+  	@film_id = params[:id];
   	@film_detail = Tmdb::Movie.detail(@film_id);
   	@film_trailer = Tmdb::Movie.trailers(@film_id);
   	@film_similaire = Tmdb::Movie.similar_movies(@film_id);
   	@film_image = Tmdb::Movie.images(@film_id);
-  	@num_trailer = 2;
-  	@num = 0;
-  	@source = "";
+  	@num_trailer = params[:source];
   end
 
   def suggestion
