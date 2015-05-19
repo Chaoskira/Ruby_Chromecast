687839a098a3d557911c92514bdd2e447cabbf98
Gordfang <Baptiste.cazaubon@gmail.com>
2015-05-19 09:45:21 +0200

    Modification des variables générale

--- app/controllers/thrailer_controller.rb
+++ app/controllers/thrailer_controller.rb
@@ -1,9 +1,9 @@
 class ThrailerController < ApplicationController
   def jouer
-  	@film_detail = Tmdb::Movie.detail(params[:id]);
-  	@film_trailer = Tmdb::Movie.trailers(params[:id]);
-  	@film_similaire = Tmdb::Movie.similar_movies(params[:id]);
-  	@film_image = Tmdb::Movie.images(params[:id]);
+  	@film_detail = Tmdb::Movie.detail(177572);
+  	@film_trailer = Tmdb::Movie.trailers(177572);
+  	@film_similaire = Tmdb::Movie.similar_movies(177572);
+  	@film_image = Tmdb::Movie.images(177572);
   end
 
   def suggestion
