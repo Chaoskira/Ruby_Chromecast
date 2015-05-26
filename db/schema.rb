15cd93cd9eb290f6b07da96b03a16a05500cf546
Gordfang <Baptiste.cazaubon@gmail.com>
2015-05-26 12:11:04 +0200

    Passage de sqlite à postgresql (et justification du texte)

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150513132724) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "administrateurs", force: :cascade do |t|
     t.string   "pseudo"
     t.string   "password"
